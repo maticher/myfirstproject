194c0e84f019b980dbcd2302aa925a0db330a970
maticher <matias.maharishi@gmail.com>
2019-03-16 18:19:02 -0500

    creating config server and client

---
+++ config-client/src/main/java/com/mati/config/configclient/MessageResource.java
@@ -0,0 +1,16 @@
+package com.mati.config.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/rest")
+@RestController
+public class MessageResource {
+	@Value("${message: Default Hello}")
+	private String message;
+@GetMapping("/message")
+public String message() {
+	return message;
+}
+}
